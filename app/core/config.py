38c0d064da37a063afe3a73c59f375e054f5482d
Miguel Angel Piccato <pmiguel2008@hotmail.es>
2026-03-23 16:33:22 -0300

    chore: align config and sqlalchemy base

--- app/core/config.py
+++ app/core/config.py
@@ -5,12 +5,11 @@ class Settings(BaseSettings):
 
     app_name: str = "Kala Turnos"
 
-    # Database
+    postgres_host: str = "localhost"
+    postgres_port: int = 5432
+    postgres_db: str = "turnero"
     postgres_user: str = "turnero"
     postgres_password: str = "turnero"
-    postgres_db: str = "turnero"
-    postgres_port: int = 5432
-    postgres_host: str = "localhost"
 
     @property
     def database_url(self) -> str:
@@ -18,5 +17,6 @@ class Settings(BaseSettings):
             f"postgresql+psycopg2://{self.postgres_user}:{self.postgres_password}"
             f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
         )
+
 
 settings = Settings()
